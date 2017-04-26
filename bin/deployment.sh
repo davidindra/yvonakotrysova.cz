4258aee26381189970a56b7ee23cdd6fd8c317e3
David Indra <mail@davidindra.cz>
2017-04-26 19:30:18 +0200

    Added deployment script

---
+++ bin/deployment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo 'Deploying...'
+cd /var/www/cmgbufet.cz
+
+rm -r temp/cache/*
+
+composer install --no-dev --no-interaction 2>&1;
+#npm install
+#bower install
+#grunt
+
+#php www/index.php orm:schema-tool:update --force
+
+echo 'Deployment finished successfully.'
