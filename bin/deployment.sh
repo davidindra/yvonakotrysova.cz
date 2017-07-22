4913b7aec5cd04386d783be1a768d7221e0ec499
David Indra <mail@davidindra.cz>
2017-07-22 20:51:48 +0200

    PHP version compatibility with server

--- bin/deployment.sh
+++ bin/deployment.sh
@@ -5,11 +5,11 @@ cd /var/www/yvonakotrysova.cz
 
 rm -r temp/cache/*
 
-composer install --no-dev --no-interaction 2>&1;
+/usr/bin/php7.0 /bin/composer install --no-dev --no-interaction 2>&1;
 npm install
 bower install
 grunt
 
-php7.0 www/index.php orm:schema-tool:update --force
+php7.0 www/index.php orm:schema-tool:update --force --dump-sql
 
 echo 'Deployment finished successfully.'
