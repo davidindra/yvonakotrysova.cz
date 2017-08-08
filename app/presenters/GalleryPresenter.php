6b0553e931331489af86fa140407c9596b1ae8ed
David Indra <mail@davidindra.cz>
2017-08-08 22:26:58 +0200

    Image multiupload

--- app/presenters/GalleryPresenter.php
+++ app/presenters/GalleryPresenter.php
@@ -34,7 +34,9 @@ class GalleryPresenter extends BasePresenter
 
         $form->addHidden('galleryId');
 
-        $form->addUpload('image', 'Fotografie:');
+        $form->addMultiUpload('image', 'Fotografie:')
+            ->addRule(Form::MAX_FILE_SIZE, 'Maximální velikost souboru je 20 MB.', 20000 * 1024)
+            ->addRule(Form::FILLED, 'Zvolte, prosím, nějaký soubor k přidání.');
 
         $form->addSubmit('send', 'Přidat do fotogalerie');
 
@@ -44,47 +46,49 @@ class GalleryPresenter extends BasePresenter
                 $this->error('Musíte být přihlášen/a jako administrátor.');
             }
 
-            $file = $values['image'];
+            foreach($values['image'] as $file) {
 
-            if ($file->isImage() && $file->isOk()) {
+                if ($file->isImage() && $file->isOk()) {
 
-                $extension = strtolower(mb_substr($file->getSanitizedName(), strrpos($file->getSanitizedName(), ".")));
+                    $extension = strtolower(mb_substr($file->getSanitizedName(), strrpos($file->getSanitizedName(), ".")));
 
-                $unique = uniqid(rand(0, 20), TRUE);
+                    $unique = uniqid(rand(0, 20), TRUE);
 
-                $fileName = $unique . $extension;
-                $pathWww = '/img/gallery/' . $fileName;
-                $pathApp = __DIR__ . '/../../www' . $pathWww;
+                    $fileName = $unique . $extension;
+                    $pathWww = '/img/gallery/' . $fileName;
+                    $pathApp = __DIR__ . '/../../www' . $pathWww;
 
-                $fileNameThumb = $unique . '.thumb' . $extension;
-                $pathWwwThumb = '/img/gallery/' . $fileNameThumb;
-                $pathAppThumb = __DIR__ . '/../../www' . $pathWwwThumb;
+                    $fileNameThumb = $unique . '.thumb' . $extension;
+                    $pathWwwThumb = '/img/gallery/' . $fileNameThumb;
+                    $pathAppThumb = __DIR__ . '/../../www' . $pathWwwThumb;
 
-                if(!is_dir(pathinfo($pathApp)['dirname'])) mkdir(pathinfo($pathApp)['dirname']);
+                    if (!is_dir(pathinfo($pathApp)['dirname'])) mkdir(pathinfo($pathApp)['dirname']);
 
-                $file->move($pathApp);
+                    $file->move($pathApp);
 
-                $image = Image::fromFile($pathApp);
-                if ($image->getWidth() > $image->getHeight()) {
-                    $image->resize(300, NULL);
+                    $image = Image::fromFile($pathApp);
+                    if ($image->getWidth() > $image->getHeight()) {
+                        $image->resize(300, NULL);
+                    } else {
+                        $image->resize(NULL, 225);
+                    }
+                    $image->sharpen();
+                    $image->save($pathAppThumb);
+
+                    $photo = new Photo();
+                    $photo->setOwningGallery($this->galleries->getById($values['galleryId']));
+                    $photo->setOrder($this->photos->getLastId() + 1);
+                    $photo->setSource($pathWww);
+                    $photo->setSourcePreview($pathWwwThumb);
+
+                    $this->photos->add($photo);
+
+                    //$this->redirect('this');
+
                 } else {
-                    $image->resize(NULL, 225);
+                    $this->error('Nahrání fotografie selhalo.', Nette\Http\IResponse::S500_INTERNAL_SERVER_ERROR);
                 }
-                $image->sharpen();
-                $image->save($pathAppThumb);
 
-                $photo = new Photo();
-                $photo->setOwningGallery($this->galleries->getById($values['galleryId']));
-                $photo->setOrder($this->photos->getLastId() + 1);
-                $photo->setSource($pathWww);
-                $photo->setSourcePreview($pathWwwThumb);
-
-                $this->photos->add($photo);
-
-                //$this->redirect('this');
-
-            }else{
-                $this->error('Nahrání fotografie selhalo.', Nette\Http\IResponse::S500_INTERNAL_SERVER_ERROR);
             }
         };
 
